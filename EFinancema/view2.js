062b36e4b48d25d68cab0481b27eb539150a9ba0
zhujubin <954338536@qq.com>
2017-11-21 15:37:43 +0800

    update css and html

--- EFinancema/view2.js
+++ EFinancema/view2.js
@@ -1,4 +1,5 @@
-define(['text!EFinancema/check_attachment.html'], function (tpl) {
+define(['text!EFinancema/check_attachment.html','text!EFinancema/general_manager_do.html','text!EFinancema/write_opinion.html'],
+    function (tpl,general,write) {
 
     var View2 = Backbone.View.extend({
         el: '#device_content',
@@ -6,7 +7,10 @@ define(['text!EFinancema/check_attachment.html'], function (tpl) {
             console.log("’‚ «check_attachment");
         },
         render: function () {
-            this.$el.html(_.template(tpl));
+            console.log(write);
+            this.$el.html(write);
+            var c = _.template(write);
+            this.$el.html(c({name:"lingxiao"}));
         }
     });
     return View2;
